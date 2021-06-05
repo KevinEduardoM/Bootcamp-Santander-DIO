233bbe709da0d8306617d3f47accb1d1c8e824b4
Kevin Eduardo Martins <76890492+KevinEduardoM@users.noreply.github.com>
2021-06-04 22:04:52 -0300

    Update BootcampApplication.java

--- src/main/java/com/project/Bootcamp/BootcampApplication.java
+++ src/main/java/com/project/Bootcamp/BootcampApplication.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 @SpringBootApplication
 public class BootcampApplication {
 
